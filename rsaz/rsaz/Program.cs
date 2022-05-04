d591dc8249af3573e6aca1ac5cf5f06a4cb3618b
JKiskisl <smicscl@gmail.com>
2022-05-04 18:26:29 +0300

    initiv2

---
+++ rsaz/rsaz/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Numerics;
+using System.Threading.Tasks;
+
+namespace rsaz
+{
+    class Program
+    {
+        public static void Txt()
+        {
+            Console.WriteLine("Choose an option: ");
+            Console.WriteLine("1. Show n, fi(n), e, public key, d");
+            Console.WriteLine("2. Encrypt");
+            Console.WriteLine("3. Decrypt");
+            Console.WriteLine("9. exit");
+        }
+
+
+        static void Main(string[] args)
+        {
+            RSA rsa = new RSA();
+
+            Console.WriteLine("Iveskite q: ");
+            int q = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Iveskite p: ");
+            int p = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter msg: ");
+            String msg = Console.ReadLine();
+
+            BigInteger n = rsa.getN(q, p);
+            BigInteger Fin = rsa.getFi(q, p);
+            BigInteger e = rsa.getPrime(rsa.rnd, Fin);
+            BigInteger d = rsa.getD(e, Fin);
+
+            Txt();
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            while (option!=9)
+            {
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("N: " + n);
+                        Console.WriteLine("Fi(N): " + Fin);
+                        Console.WriteLine("e: " + e);
+                        Console.WriteLine("d: " + d);
+                        Console.WriteLine("public key: (" + n + ", " + e + ")");
+                        break;
+                    case 2:
+                        Console.WriteLine(rsa.getEncriptedText(msg));
+                        break;
+                    case 3:
+                        Console.WriteLine(rsa.getDecryptedText(rsa.getEncriptedText(msg)));
+                        break;
+                    default:
+                        break;
+                }
+                Txt();
+                option = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("n: " + n);
+            Console.WriteLine("Fi(N): "+ Fin);
+            Console.WriteLine("e: "+ e);
+            Console.WriteLine("-------");
+            Console.WriteLine("Public key: (" + n + " " + e + ")");
+            Console.WriteLine("d: " + d);
+
+            Console.WriteLine(rsa.getEncriptedText(msg));
+
+            Console.WriteLine(rsa.getDecryptedText(rsa.getEncriptedText(msg)));
+
+
+/*            Console.WriteLine(rsa.getEncriptedText("labas"));
+
+            Console.WriteLine(rsa.getDecryptedText(rsa.getEncriptedText("labas")));*/
+        }
+    }
+}
---
+++ rsaz/rsaz/RSA.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rsaz
+{
+    class RSA
+    {
+        private char blockSeporator = '*';
+        private static List<int> arr;
+        public Random rnd;
+        private BigInteger e;
+        private BigInteger d;
+        private BigInteger n;
+
+        public RSA()
+        {
+            this.initParams();
+        }
+
+        public RSA(char blockSeporator)
+        {
+            this.blockSeporator = blockSeporator;
+            this.initParams();
+        }
+
+        private void initParams()
+        {
+            arr = initPrimeNum();
+            rnd = new Random();
+            generatePQ();
+        }
+
+        private List<int> initPrimeNum()
+        {
+            List<int> arr = new List<int>();
+            int low = 10000, high = 20000, i, flag;
+            while (low < high)
+            {
+                flag = 0;
+
+                for (i = 2; i <= low / 2; ++i)
+                {
+                    if (low % i == 0)
+                    {
+                        flag = 1;
+                        break;
+                    }
+                }
+
+                if (flag == 0)
+                    arr.Add(low);
+
+                ++low;
+            }
+            return arr;
+        }
+
+        private void getPrimeNum(Random rnd, List<int> arr, out int n1, out int n2)
+        {
+            int firstNumber = arr[rnd.Next(arr.Count)];
+            int secondNumber = arr[rnd.Next(arr.Count)];
+            if (firstNumber == secondNumber)
+            {
+                this.getPrimeNum(rnd, arr, out n1, out n2);
+            }
+            else
+            {
+                n1 = firstNumber;
+                n2 = secondNumber;
+            }
+        }
+
+        public BigInteger getN(int p, int q)
+        {
+            return p * q;
+        }
+
+        public BigInteger getFi(int p, int q)
+        {
+            return (p - 1) * (q - 1);
+        }
+
+        private BigInteger NOD(BigInteger a, BigInteger b)
+        {
+            if (a == b)
+                return a;
+            else
+                if (a > b)
+                return NOD(a - b, b);
+            else
+                return NOD(b - a, a);
+        }
+
+        public BigInteger getPrime(Random rnd, BigInteger fi)
+        {
+            BigInteger e = rnd.Next(1, Math.Abs((int)fi));
+            do
+            {
+                if (NOD(e, fi) == 1) break;
+                else e++;
+            } while (true);
+            if (e >= fi)
+            {
+                e--;
+                do
+                {
+                    if (NOD(e, fi) == 1) break;
+                    else e--;
+                } while (true);
+            }
+            return e;
+        }
+
+        public BigInteger getD(BigInteger e, BigInteger fi)
+        {
+            BigInteger i = fi, v = 0, d = 1;
+            while (e > 0)
+            {
+                BigInteger t = i / e, x = e;
+                e = i % x;
+                i = x;
+                x = d;
+                d = v - t * x;
+                v = x;
+            }
+            v %= fi;
+            if (v < 0) v = (v + fi) % fi;
+            return v;
+        }
+
+        private string encript(int symbol, BigInteger keyE, BigInteger keyN)
+        {
+            return BigInteger.ModPow(symbol, keyE, keyN).ToString();
+        }
+
+        private int decript(int symbol, BigInteger keyD, BigInteger keyN)
+        {
+            return (int)(BigInteger.ModPow(symbol, keyD, keyN));
+        }
+
+        private string textEncript(string text, BigInteger e, BigInteger n)
+        {
+            string eText = "";
+            foreach (var symbol in text)
+            {
+                eText += encript(symbol, e, n) + this.blockSeporator;
+            }
+            return eText;
+        }
+
+        private string textDecript(string text, BigInteger d, BigInteger n)
+        {
+            string[] arr = text.Split(this.blockSeporator);
+            string dText = "";
+            foreach (var symbol in arr)
+            {
+                if (symbol != String.Empty)
+                    dText += Convert.ToChar(decript(int.Parse(symbol), d, n));
+            }
+            return dText;
+        }
+
+        public string getEncriptedText(string originalText)
+        {
+            return textEncript(originalText, this.e, this.n);
+        }
+
+        public string getDecryptedText(string encryptedText)
+        {
+            return textDecript(encryptedText, this.d, this.n);
+
+        }
+
+        private void generatePQ()
+        {
+            int q;
+            int p;
+            getPrimeNum(rnd, arr, out q, out p);
+            BigInteger fi = getFi(p, q);
+            this.n = getN(p, q);
+            this.e = getPrime(rnd, fi);
+            this.d = getD(e, fi);
+        }
+    }
+
+}
+
